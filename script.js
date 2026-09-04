1f867e7337da5013920b17bf9c01ac9d01c4e4a6
Saroj Moharana <112850451+Saroj-969@users.noreply.github.com>
2026-09-04 13:49:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+
+btn2.addEventListener("click",()=>{
+    window.open("https://github.com/Saroj-969", '_blank'); 
+})
+btn1.addEventListener("click",()=>{
+     window.open("/resume.pdf", "_blank");
+})
